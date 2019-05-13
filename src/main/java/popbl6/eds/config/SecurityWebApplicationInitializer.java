0e5b121e994c6c65910cc62b0f0fad6cbde4a8f6
littlesafra <elena.larreategui@alumni.mondragon.edu>
2019-05-13 20:13:42 +0200

    Starting with the chat, pom ok

--- src/main/java/popbl6/eds/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package popbl6.eds.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-  extends AbstractSecurityWebApplicationInitializer {
-
-}
